274cd75046881a0d9873dbecae4d3291601c9c53
gonza <gonza56d@gmail.com>
2021-01-17 17:04:50 -0300

    Base Scrapper that rocks!!!!!!!!

--- scrappers/angelco/angel_co.py
+++ scrappers/angelco/angel_co.py
@@ -1,45 +1,15 @@
 """Implementation of AngelCoPage"""
 
-# Python <3
-import csv, time, traceback
-
 # Project
 from pages import AngelCoPage
+from scrappers.base_scrapper import BaseScrapper
 
 
-class AngelCoScrapper:
+class AngelCoScrapper(BaseScrapper):
 
     page_class = AngelCoPage
-    count = 0
-    begin = 0
-    end = 0
+    headers = ['name', 'description', 'location', 'area', 'investments', 'followers']
 
     def __init__(self, executable_path):
-        self.begin = time.time()
         self.page = self.page_class(executable_path)
-        try:
-            self.begin_scrap()
-        except Exception:
-            self.finish()
-            traceback.print_exc()
-
-    def begin_scrap(self):
-        with open('result.csv', mode='w') as result:
-            result = csv.writer(result, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-            result.writerow(['Names:', 'Description:', 'Location', 'Area', 'Investments:', 'Followers:'])
-            rows = self.page.get_rows()
-            while True:
-                try:
-                    row = next(rows)
-                    self.count += 1
-                    print(f'Writing line: {self.count}')
-                    result.writerow([row.name, row.description, row.location, row.area, row.investments, row.followers])
-                except StopIteration:
-                    break
-            self.finish()
-
-    def finish(self):
-        self.page.tear_down()
-        self.end = time.time()
-        print('_________________________')
-        print(f'Scrap finished with {self.count} lines in {(self.end - self.begin) % 60} seconds. See you next time! :)')
+        super().__init__(self.page, self.headers)
---
+++ scrappers/base_scrapper.py
@@ -0,0 +1,43 @@
+# Python <3
+import csv
+import time
+import traceback
+
+
+class BaseScrapper:
+    """Base scrapper class to inherit in page scrappers."""
+
+    count = 0
+    begin = 0
+    end = 0
+
+    def __init__(self, page, headers):
+        self.begin = time.time()
+        self.page = page
+        self.headers = headers
+        try:
+            self.begin_scrap()
+        except Exception:
+            self.finish()
+            traceback.print_exc()
+
+    def begin_scrap(self):
+        with open('result.csv', mode='w') as result:
+            result = csv.writer(result, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+            result.writerow([header.capitalize()+':' for header in self.headers])
+            rows = self.page.get_rows()
+            while True:
+                try:
+                    row = next(rows)
+                    self.count += 1
+                    print(f'Writing line: {self.count}')
+                    result.writerow([getattr(row, header) for header in self.headers])
+                except StopIteration:
+                    break
+            self.finish()
+
+    def finish(self):
+        self.page.tear_down()
+        self.end = time.time()
+        print('_________________________')
+        print(f'Scrap finished with {self.count} lines in {(self.end - self.begin) % 60} seconds. See you next time! :)')
