a3a50a88177cfd6afb890d0530b7760e81a642da
gonza <gonza56d@gmail.com>
2021-01-18 21:55:55 -0300

    Runner scrapper import cleaner

--- runner.py
+++ runner.py
@@ -1,9 +1,9 @@
 # Pages
-from scrappers import AngelCoScrapper, CapterraComScrapper
+import scrappers
 
 
 executable_path = './chromedriver'
 
 
 if __name__ == '__main__':
-    runner = CapterraComScrapper(executable_path)
+    runner = scrappers.CapterraComScrapper(executable_path)
